23f209ea89f0cd2f4ffd0b79276ea4d6fad57536
Zainab6521 <zk.652162@gmail.com>
2021-06-08 04:05:06 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
-import useHttp from './hooks/use-https';
+import useHttp from './hooks/use-http';
 
 function App() {
   const [tasks, setTasks] = useState([]);
--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,13 +1,11 @@
-import { useState } from "react";
-
-import Section from "../UI/Section";
-import TaskForm from "./TaskForm";
-import useHttp from "../../hooks/use-https";
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
   const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
 
-  const createTask = (taskData, taskText) => {
+  const createTask = (taskText, taskData) => {
     const generatedId = taskData.name; // firebase-specific => "name" contains generated id
     const createdTask = { id: generatedId, text: taskText };
 
@@ -17,15 +15,15 @@ const NewTask = (props) => {
   const enterTaskHandler = async (taskText) => {
     sendTaskRequest(
       {
-        Url: "https://react-http-d15e2-default-rtdb.firebaseio.com//tasks.json",
-        method: "POST",
+        url: 'https://react-http-d15e2-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: {text: taskText}
+        body: { text: taskText },
       },
       createTask.bind(null, taskText)
-    ); 
+    );
   };
 
   return (
